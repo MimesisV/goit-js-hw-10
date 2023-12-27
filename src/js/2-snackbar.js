bb35cd2e4511524b94581f6c27572fe79b0ac7c3
MimesisV <141736479+MimesisV@users.noreply.github.com>
2023-12-27 13:44:41 +0200

    Task 1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,5 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
+
+const input = document.querySelector('input[name="delay"]');
+const button = document.querySelector('button');
